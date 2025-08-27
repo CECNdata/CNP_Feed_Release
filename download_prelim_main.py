8f8691282ac47f4b9a6a51f90b8540b8cdb54c7b
eloco <22910374+Eloco@users.noreply.github.com>
2025-08-27 18:52:52 +0800

    modified:   .github/workflows/CNP-Customs-prelim.yaml 	modified:   download_prelim_main.py

--- download_prelim_main.py
+++ download_prelim_main.py
@@ -57,7 +57,7 @@ def save(page,fullTitle,filename):
         f.write(page.content())
 
 def run(playwright):
-    browser = playwright.firefox.launch(headless=True)
+    browser = playwright.firefox.launch(headless=False)
     context = browser.new_context(
         viewport={"width": 1920, "height": 1080},
         user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 "
