adf8e1b604f07f51d2e7a05ee4291d9591f3351d
CECNdata <94894220+CECNdata@users.noreply.github.com>
2026-03-12 09:33:25 +0800

    Update download_prelim_main.py

--- download_prelim_main.py
+++ download_prelim_main.py
@@ -82,7 +82,7 @@ def run(playwright):
     
     page.goto("http://www.customs.gov.cn/customs/302249/zfxxgk/2799825/302274/302277/4185050/index.html",wait_until="domcontentloaded")
     page.wait_for_timeout(5000)
-    page.locator("""xpath=(//div[@class="tjYear"]//a)[1]""").click() ##  click the latest year
+    page.locator("""xpath=(//li/a[contains(text(),'20')])[1]""").click() ##  click the latest year
     page.wait_for_timeout(5000)
 
     from urllib.parse import urljoin
